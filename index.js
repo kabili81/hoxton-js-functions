e90f72793ace285a99957222e58415d26e5f9696
Kabil Ibrahimi <kabili81@hotmail.com>
2022-06-20 09:02:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+//Instructions
+//- Use this starting template, you'll find the functions already there => https://codesandbox.io/s/day-5-functions-template-19u58?file=/index.js
+//- Again, do not code your solution in CodeSandbox. Copy the stuff over to your project!
+//- Write code that solves the requirements
+//- Test your solution by giving it some sample data and console.log the results 
+
+/* WRITE THE CODE FOR THE FOLLOWING FUNCTIONS */
+
+Input: a (2), b (4)
+Action: (a, b)
+Output: The result (number)
+function add(a, b) {
+   return a + b
+  }
+  
+  // Input: number
+  // Action: Check if the number given is even or not
+  // Output: The result (boolean)
+  function isEven() {
+    // write your code here
+  }
+  
+  // Input: name (string)
+  // Action: Greet the user by name
+  // Output: The greeting (string)
+  function greet() {
+    // write your code here
+  }
+  
+  // Input: age (number)
+  // Action: Check if the age is 18 or over
+  // Output: The result (boolean)
+  function isAnAdult() {
+    // write your code here
+  }
+  
+  // Input: age (number)
+  // Action: Check how many years are left until adulthood (18)
+  // Output: The result (number)
+  function yearsToAdulthood() {
+    // write your code here
+  }
+  
+  // Input: person ({ age: number, name: string })
+  // Action:
+  //   - check if a person is an adult
+  //   - if they are, greet them
+  //   - if they are not, tell them to come back in X years (when they are)
+  // Output: The result (string)
+  function admit() {
+    // write your code here
+    // use greet, isAnAdult and yearsToAdulthood to help you!
+  }
+  
